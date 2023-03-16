770e3f7688ae8f353cfb423d2c65045e42791790
Mohan Jadhav <32812918+codexhunter4u@users.noreply.github.com>
2023-03-16 11:15:58 +0530

    Update Module.php
    self review

--- module/User/src/Module.php
+++ module/User/src/Module.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace User;
 
-
 use User\Controller\UserController;
 use User\Controller\UserControllerFactory;
 use Laminas\ModuleManager\Feature\ConfigProviderInterface;
